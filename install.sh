301d9dcb2d81d491b3854d0dfc7c3ad16e6afaca
Evgeny Gorbachev <djonsilver@yandex.ru>
2020-02-28 15:52:05 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sh linux-amd64_deb/install.sh     && wget https://update.cryptopro.ru/support/nginx-gost/bin/180423/cprocsp-cpopenssl-110-gost-64_5.0.11216-5_amd64.deb \
+       -O /mnt/cprocsp-cpopenssl-110-gost-64_5.0.11216-5_amd64.deb \
+    && dpkg -i /mnt/cprocsp-cpopenssl-110-gost-64_5.0.11216-5_amd64.deb
