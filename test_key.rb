5b036118b754910600d4db96e9fbda725b0b8688
Evgeny Gorbachev <djonsilver@yandex.ru>
2020-05-18 18:22:37 +0300

    commands to test installation and keys test script

---
+++ test_key.rb
@@ -0,0 +1,16 @@
+require 'openssl'
+OpenSSL::Engine.load
+engine = OpenSSL::Engine.by_id('gost')
+engine.set_default(0xFFFF)
+
+
+cert   = OpenSSL::X509::Certificate.new File.binread('config/keys/gost_keys/certificate.crt')
+pkey   = OpenSSL::PKey.read(File.read('config/keys/gost_keys/private.key'))
+# , "79401194")
+digester  = engine.digest('md_gost12_256')
+
+data = 'dfsdfsdfsdfsdf'
+
+signature = pkey.sign(digester, data)
+puts "Signature #{signature}"
+puts "Verified? - #{cert.public_key.verify(digester, signature, data)}"
