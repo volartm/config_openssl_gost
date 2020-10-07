5a023f72ffcd4133ec32fe783bf43746b03bcd74
Gorbachev Evgeny <volartm@users.noreply.github.com>
2020-10-07 23:12:01 +0300

    Update test_key.rb

--- test_key.rb
+++ test_key.rb
@@ -6,7 +6,7 @@ engine.set_default(0xFFFF)
 
 cert   = OpenSSL::X509::Certificate.new File.binread('config/keys/gost_keys/certificate.crt')
 pkey   = OpenSSL::PKey.read(File.read('config/keys/gost_keys/private.key'))
-# , "79401194")
+
 digester  = engine.digest('md_gost12_256')
 
 data = 'dfsdfsdfsdfsdf'
