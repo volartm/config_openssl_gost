61f573f57ec641df7369788f226ef0f855593a1e
Gorbachev Evgeny <volartm@users.noreply.github.com>
2020-10-07 22:52:14 +0300

    Update 2_install_gem_openssl_patched.sh

--- install/done/2_install_gem_openssl_patched.sh
+++ install/done/2_install_gem_openssl_patched.sh
@@ -1,7 +1,7 @@
 PREFIX="/usr/local/ssl"
 
 # installing 
-echo 'gem "openssl",  git: "https://gitlab+deploy-token-10:jz8m25znE4k6qpSvyPKE@git.ceki.ru/ceki/openssl_patched.git"' >> Gemfile
+echo 'gem "openssl",  git: "git@github.com:volartm/openssl_patched.git"' >> Gemfile
 
 rvm use ruby-2.4.1
 
